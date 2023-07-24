0bb126062bd469ee06e18492ecf73384085ede36
silver1124 <136788502+silver1124@users.noreply.github.com>
2023-07-24 10:54:47 +0530

    Forth commit

--- Expense/ExpenseItem.js
+++ Expense/ExpenseItem.js
@@ -7,13 +7,16 @@ import Card from '../UI/Card';
 
 
 const ExpenseItem = (props) => {
-
+    const clickhandler = ()=> {
+        console.log("Clicked!!")
+    }
     return (
         <Card className = "expense-item">
             <div className='temp'>
             <ExpenseDate date= {props.date}/>
             <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
-            </div>
+            <button onClick = {clickhandler} >Delete Expense</button>
+            </div>            
         </Card>
     ) 
 }
