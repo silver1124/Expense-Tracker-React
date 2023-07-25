2079c0b9ac8e21589c3390cbe7d87b05c1510a1e
silver1124 <136788502+silver1124@users.noreply.github.com>
2023-07-25 14:53:16 +0530

    Seventh commit

---
+++ ExpenseForm.js
@@ -0,0 +1,44 @@
+
+import React, { useState } from 'react';
+import './ExpenseForm.css';
+
+function ExpenseForm (){
+
+    const [enteredtitle, setenteredtitle] = useState('');
+    const [enteredamount, setenteredamount] = useState('');
+    const [entereddate, setentereddate] = useState('');
+
+    const titleChangehandler = (event) => {
+        setenteredtitle(event.target.value);
+    };
+    const amountChangehandler = (event) => {
+        setenteredamount(event.target.value);
+    };
+    const dateChangehandler = (event) => {
+        setentereddate(event.target.value);
+    };
+
+
+    return (
+        <form>
+            <div className='new-expense__controls'>
+                <div className='new-expense__control'>
+                    <label>Expense Title</label>
+                    <input type="text" onChange={titleChangehandler} /> 
+                </div>
+                <div className='new-expense__control'>
+                    <label>Expense Amount</label>
+                    <input type="number" onChange={amountChangehandler} /> 
+                </div>
+                <div className='new-expense__control'>
+                    <label>Expense Date</label>
+                    <input type="date" onChange={dateChangehandler} /> 
+                </div>
+            </div>
+            <div className='new-expense__actions'>
+                <button type = "submit">Add Expense</button>
+            </div>
+        </form>);
+}
+
+export default ExpenseForm
